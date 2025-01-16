897fdd68b00a576699f2847ceee2fb9792801467
vandanafuloria <vandanafuloria02@gmail.com>
2025-01-16 17:22:20 +0530

    Added functions which set the images according to option choose

--- script.js
+++ script.js
@@ -1,26 +1,49 @@
-const btnEle = document.querySelector('button');
-const optionEl= document.querySelector('.options')
-// console.log(btnEle);
+const btnEl = document.querySelector('.button');
+console.log (btnEl);
+const optionEl = document.querySelector('.player')
+const computerBot = document.querySelector('.bot');
+const rock = document.querySelector('.rock');
+console.log(rock);
+const paper = document.querySelector('.paper');
+console.log(paper);
+const scissor = document.querySelector('.scissor');
+ // console.log(btnEle);
 // console.log(btnEle.innerText);
 
 function playButton(){
-    console.log(btnEle.innerText);
+   
     const random = Math.random() * (4 - 1) + 1;
-    // console.log("hello", random)
     const round = Math.floor(random);
-    // console.log(rock);
-    // console.log(rock.innerText);
-    // console.log(rock.innerText="hello");
-   const img = "images/" + round + ".jpg";
-   console.log(img);
+    const img = "images/" + round + ".jpg";
+
     optionEl.setAttribute('src',img)
     console.log(optionEl.innerText);
 
-    
-    // console.log(random, round);
-    
+    const randomNum  = Math.random() * (4 - 1) + 1;
+    const roundNum = Math.floor(randomNum);
+    const imageForComptuter = "images/" + roundNum + ".jpg";
+    computerBot.setAttribute('src',imageForComptuter )
+
 }
- 
+
+function optionDisplayRock(){
+    const rockImage = "images/" + 3 + ".jpg";
+    optionEl.setAttribute('src' ,rockImage );
+}
+function optionDisplayPaper(){
+    const paperImage = "images/" + 2 + ".jpg";
+    optionEl.setAttribute('src' ,paperImage );
+}
+function optionDisplayScissor(){
+    const scissorImage = "images/" + 1 + ".jpg";
+    optionEl.setAttribute('src' ,scissorImage );
+}
+ // need to set class atrribut on clicking a perticular element
 // btnEle.onclick() = playButton;
-btnEle.addEventListener('click', playButton);
+btnEl.addEventListener('click', playButton);
 // btnEle.onclick = playButton;
+rock.addEventListener('click',optionDisplayRock );
+
+paper.addEventListener('click', optionDisplayPaper);
+ 
+scissor.addEventListener('click', optionDisplayScissor);
