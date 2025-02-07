e3a43682d71612506dc11ff5d5f1853a7a987d68
vandanafuloria <vandanafuloria02@gmail.com>
2025-02-07 14:32:01 +0530

    correct output b/w players functions added

--- script.js
+++ script.js
@@ -1,14 +1,79 @@
 const btnEl = document.querySelector('.button');
-console.log (btnEl);
 const optionEl = document.querySelector('.player')
 const computerBot = document.querySelector('.bot');
 const rock = document.querySelector('.rock');
-console.log(rock);
 const paper = document.querySelector('.paper');
-console.log(paper);
 const scissor = document.querySelector('.scissor');
+const boardEl = document.querySelector('#notice')
  // console.log(btnEle);
 // console.log(btnEle.innerText);
+
+
+function generateComputerChoice() {
+    const random = Math.random() * (4 - 1) + 1;
+    const round = Math.floor(random);
+    return round;
+}
+
+/*
+1-> scissor
+2-> Paper
+3-> rock
+
+-> 'draw', 'computer', 'user'
+*/
+function calculateWinner(compChoice, userChoice) {
+
+    if(compChoice == userChoice) return 'draw';
+
+    switch(compChoice) {
+        case 1:
+            
+            if(userChoice == 2) return 'computer'
+            else return 'user'
+
+        case 2:
+            return userChoice == 1 ? 'user' : 'computer'
+
+        case 3:
+            return userChoice == 1 ? 'computer' : 'user'
+    }
+}
+
+
+
+function playGame(userChoice) {
+    // user choice: rock
+    const image = "images/" + userChoice + ".jpg";
+    console.log("setting imager to " + image);
+    optionEl.setAttribute('src' ,image );
+    
+    // computer choice: 
+    const computerChoice = generateComputerChoice();
+    const choiceImage = "images/" + computerChoice + ".jpg";
+    computerBot.setAttribute('src', choiceImage);
+
+    const winner = calculateWinner(computerChoice, userChoice);
+    console.log("winner: " + winner)
+    let message;
+    if(winner == 'draw') message = "It's a draw :(";
+    else message = (winner == 'user' ? "You" : "Bot") +  " Won"
+
+    boardEl.textContent = message;
+}
+
+
+function optionDisplayRock(){
+    playGame(3);
+}
+
+function optionDisplayPaper(){
+    playGame(2);
+}
+
+function optionDisplayScissor(){
+    playGame(1);
+}
 
 function playButton(){
    
@@ -26,18 +91,7 @@ function playButton(){
 
 }
 
-function optionDisplayRock(){
-    const rockImage = "images/" + 3 + ".jpg";
-    optionEl.setAttribute('src' ,rockImage );
-}
-function optionDisplayPaper(){
-    const paperImage = "images/" + 2 + ".jpg";
-    optionEl.setAttribute('src' ,paperImage );
-}
-function optionDisplayScissor(){
-    const scissorImage = "images/" + 1 + ".jpg";
-    optionEl.setAttribute('src' ,scissorImage );
-}
+
  // need to set class atrribut on clicking a perticular element
 // btnEle.onclick() = playButton;
 btnEl.addEventListener('click', playButton);
